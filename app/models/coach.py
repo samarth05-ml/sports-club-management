9b401fdad13c00e68452fe7b0da331ae464c672a
samarth05-ml <samarthprabhu000@gmail.com>
2026-07-12 20:04:36 +0530

    Coach database table created

---
+++ app/models/coach.py
@@ -0,0 +1,17 @@
+from ..extensions import db
+
+class Coach(db.Model):
+    __tablename__='coach'
+
+    cid=db.Column(db.Integer,primary_key=True)
+
+    user_id=db.Column(db.Integer,
+                      db.ForeignKey("user.uid"),
+                      unique=True,
+                      nullable=False)
+    team_id=db.Column(db.Integer,
+                      db.ForeignKey("team.tid"),
+                      unique=True,
+                      nullable=False)
+    user=db.relationship("User",backref='coach_profile')
+    team=db.relationship("Team",backref='coach')
